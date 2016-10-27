4ef86703155306565dfedaf9763d1cd387647284
刘广兴 <546589732@qq.com>
2016-10-27 16:54:41 +0800

    Initial Commit

---
+++ LGXScressLaber/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LGXScressLaber
+//
+//  Created by 兴哥哥 on 2016/10/27.
+//  Copyright © 2016年 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LGXScressLaber/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LGXScressLaber
+//
+//  Created by 兴哥哥 on 2016/10/27.
+//  Copyright © 2016年 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
